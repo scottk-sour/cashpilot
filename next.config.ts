b95fdfab4a5f8c164c94921986869c061d6f1289
scottk-sour <122282065+scottk-sour@users.noreply.github.com>
2025-12-03 17:00:20 +0000

    Refactor Sentry configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -40,27 +40,13 @@ const nextConfig: NextConfig = {
 };
 
 export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
   org: "cashpilot",
   project: "cashpilot-web",
-
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers
   tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  sourcemaps: {
+    disable: true,
+  },
   disableLogger: true,
 });
